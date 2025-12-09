2b0e0434987623952da27c662a1c8fc76ed5ea30
Admin <=>
2025-12-10 01:14:00 +0200

    python_load_files

--- python_scr/main.py
+++ python_scr/main.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
 import sqlalchemy as sql
+import matplotlib.pyplot as plt
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 2000)
 pd.set_option('display.expand_frame_repr', False)
 pd.set_option('display.max_colwidth', None)
+# pd.set_option('display.max_rows', 500)
 
 patterns_df = pd.read_csv('data/internet_usage.csv')
 patterns_df = patterns_df.replace("..", np.nan)
@@ -59,4 +61,46 @@ print(patterns_df.head(10))
 #         patterns_df.to_dict(orient="records")
 #     )
 #     print("patterns вставлены")
+
+############################################################### add useful data from world bank group
+
+country_reg = pd.read_excel('data/country_regions.xlsx')
+# print(country_reg)
+
+patterns_df = patterns_df.merge(country_reg, how = 'left', left_on = 'Country Name', right_on = 'Country')
+patterns_df = patterns_df.drop('Country', axis=1)
+patterns_df = patterns_df.iloc[:,[0,1,26,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]]
+#print(patterns_df)
+
+
+edu_stat = pd.read_csv('data/edu.csv')
+edu_stat = edu_stat.iloc[:816,[0,1,2,4,5,6,7,8]]
+# print(edu_stat.tail(10))
+
+edu_stat = edu_stat.melt(id_vars = ['Country Name', 'Country Code','Series'])
+edu_stat['variable'] = edu_stat['variable'].astype(str).str[:4]
+edu_stat['variable'] = pd.to_numeric(edu_stat["variable"])
+edu_stat = edu_stat.rename(columns = {'variable':'year'})
+edu_stat = edu_stat.pivot(index=['Country Name','Country Code','year'],columns='Series', values='value')
+# print(edu_stat)
+
+gpd_stat = pd.read_csv('data/GDP.csv')
+# print(gpd_stat.tail(10))
+gpd_stat = gpd_stat.iloc[:798,[0,1,2,4,5,6,7,8]]
+
+gpd_stat = gpd_stat.melt(id_vars = ['Country Name', 'Country Code','Series Name'])
+gpd_stat['variable'] = gpd_stat['variable'].astype(str).str[:4]
+gpd_stat['variable'] = pd.to_numeric(gpd_stat["variable"])
+gpd_stat = gpd_stat.rename(columns = {'variable':'year'})
+gpd_stat = gpd_stat.pivot(index=['Country Name','Country Code','year'],columns='Series Name', values='value')
+# print(gpd_stat)
+
+########################################################### EDA & Analisys
+
+patterns_melted_df = patterns_df.melt(id_vars = ['Country Name', 'Country Code','Region'])
+patterns_melted_df['value'] = patterns_melted_df['value'].astype(float)
+patterns_melted_df = patterns_melted_df.rename(columns = {'variable':'year'})
+patterns_melted_df.pivot_table(index='year',columns='Region',values='value',aggfunc='mean').plot(kind='line')
+plt.show()
+
 
