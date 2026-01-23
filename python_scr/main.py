22a957a78928a4ea8b258be3338798b4c51e2382
Admin <=>
2026-01-23 19:32:15 +0200

    update notebook

--- python_scr/main.py
+++ python_scr/main.py
@@ -70,22 +70,45 @@ patterns_df = patterns_df.replace("..", np.nan)
 
 ############################################################### add useful data from world bank group
 
-country_reg = pd.read_excel('data/country_regions.xlsx')
-# print(country_reg)
+country_reg = pd.read_excel('data/country_regions.xlsx') # add abbreviations in country names
 patterns_df = patterns_df.merge(country_reg, how = 'left', left_on = 'Country Name', right_on = 'Country')
 patterns_df = patterns_df.drop('Country', axis=1)
-#print(patterns_df)
 
 
-internet_speed = pd.read_csv('data/internet_speed.csv')
+internet_speed = pd.read_csv('data/internet_speed.csv') # additional information about internet speed
 internet_speed.rename(columns={'Region': 'Region_int'}, inplace=True)
 patterns_df = patterns_df.merge(internet_speed, how = 'left', left_on = 'Country Name', right_on = 'Region_int')
 patterns_df = patterns_df.iloc[:,[0,1,26,27,29,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]]
 
 
-edu_stat = pd.read_csv('data/edu.csv', na_values=['..'])
+edu_stat = pd.read_csv('data/edu.csv', na_values=['..']) # read education data
 edu_stat = edu_stat.iloc[:816,[0,1,2,4,5,6,7,8]]
-# print(edu_stat.tail(10))
+
+edu_stat = edu_stat.melt(id_vars = ['Country Name', 'Country Code','Series']) # data transformation for convenient work with tables
+edu_stat['variable'] = edu_stat['variable'].astype(str).str[:4]
+edu_stat['variable'] = pd.to_numeric(edu_stat["variable"])
+edu_stat = edu_stat.rename(columns = {'variable':'year'})
+edu_stat = edu_stat.pivot(index=['Country Name','Country Code','year'],columns='Series', values='value')
+
+gdp_stat = pd.read_csv('data/GDP.csv', na_values=['..']) # read GDP stats
+gdp_stat = gdp_stat.iloc[:798,[0,1,2,4,5,6,7,8]]
+
+def normolize_gdp (row): #  number transformation for readability
+    if (row['Series Name'] == 'GDP (current US$)'):
+        row['2000 [YR2000]'] = row['2000 [YR2000]']/1000000
+        row['2005 [YR2005]'] = row['2005 [YR2005]'] / 1000000
+        row['2010 [YR2010]'] = row['2010 [YR2010]'] / 1000000
+        row['2015 [YR2015]'] = row['2015 [YR2015]'] / 1000000
+        row['2020 [YR2020]'] = row['2020 [YR2020]'] / 1000000
+    return row
+gdp_stat = gdp_stat.apply(normolize_gdp, axis='columns')
+
+gdp_stat = gdp_stat.melt(id_vars = ['Country Name', 'Country Code','Series Name']) # data transformation for convenient work with tables
+gdp_stat['variable'] = gdp_stat['variable'].astype(str).str[:4]
+gdp_stat['variable'] = pd.to_numeric(gdp_stat["variable"])
+gdp_stat = gdp_stat.rename(columns = {'variable':'year'})
+gdp_stat = gdp_stat.pivot(index=['Country Name','Country Code','year'],columns='Series Name', values='value')
+
 
 # engine = sql.create_engine("postgresql://postgres:*********************************")
 # metadata = sql.MetaData()
@@ -114,27 +137,8 @@ edu_stat = edu_stat.iloc[:816,[0,1,2,4,5,6,7,8]]
 #     )
 #     print("edu вставлены")
 
-edu_stat = edu_stat.melt(id_vars = ['Country Name', 'Country Code','Series'])
-edu_stat['variable'] = edu_stat['variable'].astype(str).str[:4]
-edu_stat['variable'] = pd.to_numeric(edu_stat["variable"])
-edu_stat = edu_stat.rename(columns = {'variable':'year'})
-edu_stat = edu_stat.pivot(index=['Country Name','Country Code','year'],columns='Series', values='value')
-# print(edu_stat)
 
 
-gdp_stat = pd.read_csv('data/GDP.csv', na_values=['..'])
-gdp_stat = gdp_stat.iloc[:798,[0,1,2,4,5,6,7,8]]
-
-def normolize_gdp (row):
-    if (row['Series Name'] == 'GDP (current US$)'):
-        row['2000 [YR2000]'] = row['2000 [YR2000]']/1000000
-        row['2005 [YR2005]'] = row['2005 [YR2005]'] / 1000000
-        row['2010 [YR2010]'] = row['2010 [YR2010]'] / 1000000
-        row['2015 [YR2015]'] = row['2015 [YR2015]'] / 1000000
-        row['2020 [YR2020]'] = row['2020 [YR2020]'] / 1000000
-    return row
-gdp_stat = gdp_stat.apply(normolize_gdp, axis='columns')
-# print(gdp_stat.head(10))
 
 # engine = sql.create_engine("postgresql://postgres:*********************************")
 # metadata = sql.MetaData()
@@ -164,12 +168,7 @@ gdp_stat = gdp_stat.apply(normolize_gdp, axis='columns')
 #     print("gdp вставлены")
 
 
-gdp_stat = gdp_stat.melt(id_vars = ['Country Name', 'Country Code','Series Name'])
-gdp_stat['variable'] = gdp_stat['variable'].astype(str).str[:4]
-gdp_stat['variable'] = pd.to_numeric(gdp_stat["variable"])
-gdp_stat = gdp_stat.rename(columns = {'variable':'year'})
-gdp_stat = gdp_stat.pivot(index=['Country Name','Country Code','year'],columns='Series Name', values='value')
-# print(gdp_stat)
+
 
 ########################################################### EDA & Analisys
 
@@ -293,9 +292,8 @@ reggression_table['1/y'] = 1 / reggression_table['value']
 reggression_table['ln(X)'] = np.log(reggression_table['year'])
 reggression_table['ln(Y)'] = np.log(reggression_table['value'])
 
-reggression_table['predicted_value'] = np.nan
-
-alredy_noticed_counrtries = []
+reggression_table['predicted_value'] = np.nan # create empty col for predicted value
+alredy_noticed_counrtries = [] # list so as not to repeat
 list_of_dataframes = []
 
 def create_unique_dataframes(row):
